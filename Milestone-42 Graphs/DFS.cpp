bf53d95e3a0e445f1e01ded4ab8d0cfbb5ca5cad
Ritesh Mahato <87099919+Ritesh1625@users.noreply.github.com>
2021-12-07 12:10:08 +0530

    Update DFS.cpp

--- Milestone-42 Graphs/DFS.cpp
+++ Milestone-42 Graphs/DFS.cpp
@@ -1,1 +1,43 @@
 #include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int T;
+  cin >> T;
+  while(T--)
+  {
+    int N, E;
+    cin >> N >> E;
+    
+    vector <int> g[N];
+    bool vis[N];
+    
+    memset(vis, false, sizof(vis));
+    
+    for(int i=0; i<E; i++)
+    {
+      int u, v;
+      cin >> u>> v;
+      g[u].push_back(v);
+      g[v].push_back(u);
+    }
+    
+    dfs(0. g, vis);
+    
+    cout << endl;
+  }
+}
+
+
+void dfs(int s, vector<int>g[], bool *vis)
+{
+  vis[s]=true;
+  cout << s << " ";
+  for(int i=0; i<g[s].size(); ++i)
+  {
+    if(vis[g[s][i]==false)
+           dfs(g[s][i], g , vis);         
+  }
+ }
+  
